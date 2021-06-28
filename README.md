ca19be6b1ae73bdba72f5eeed1db5f799f3e52f2
omzi <obiohaomezibe@gmail.com>
2021-06-28 15:20:20 +0100

    Added the initialise payment function
    Completed the first part of the monetary donation BackEnd flow.

--- README.md
+++ README.md
@@ -1,22 +1,23 @@
-# Zuri project 33 backend
+### Zuri Project Team 33
+# TheZeroHungerInitiative BackEnd API
 
 ## Running the app
-start by cloning this repo in to your local machine. Followed by running `npm install` to install all the dependencies. After that type `npm run dev` to start the server.
+Start by cloning this repo in to your local machine. Followed by running `npm install` to install all the dependencies. After that, type `npm run dev` to start the server.
 
-## Application Url
-[live url](https://zero-hunger-initiative.herokuapp.com/documentation/)
+## Application URL
+[Live URL](https://zero-hunger-initiative.herokuapp.com/documentation/)
 
-## Contributingg to this project
-Markup : 1. Fork the repo.
+## Contributing to this project
+1. Fork the repo.
 
 2. Clone the fork repo into your local machine.
 ```
-git clone <the-repo-link>
+git clone https://github.com/zuri-training/TheZeroHungerInitiative-be-pjt-33.git
 ```
 
 3. Create an upstream on your local machine to pull the latest code from the main branch of the repository
 ```
-git remote add upstream <the-url-where-you-fork-this-repo>
+git remote add upstream https://github.com/zuri-training/TheZeroHungerInitiative-be-pjt-33.git
 ```
 
 4. Create a new branch on your local machine. The command below will create a new branch and then switch to the branch
@@ -40,9 +41,11 @@ switch to the branch you created on the repo. By doing that you would have seen 
 
 8. Do not merge your pull request yourself. Wait for review and merging from the coordinator.
 
+<br>
 
 ## For any subsequent changes
-Markup : 1. To makes sure your local repo is in sync with the parent repo. First you have pull the changes from the remote repo
+1. To makes sure your local repo is in sync with the parent repo. First you have pull the changes from the remote repo.
+
 ```
 git pull upstream
 ```
@@ -58,7 +61,7 @@ git push origin <branch-name>
 
 4. Come back to this repo and open a pull request on the branch you created.
 ```
-switch to the branch you created on the repo. By doing that you would have seen a button that said *compare/pull request*. press the button, which will then take you to a page to provide the title and description for the pull request. After that, then press the pull request button to submit a pull request. With that you are done, it's left for the reviewer to *review* and *merge*
+Switch to the branch you created on the repo. By doing that you would have seen a button that said *compare/pull request*. press the button, which will then take you to a page to provide the title and description for the pull request. After that, then press the pull request button to submit a pull request. With that you are done, it's left for the reviewer to *review* and *merge*
 ```
 
 5. Do not merge your pull request yourself. Wait for review and merging from the coordinator.
--- routes/donationRoute.js
+++ routes/donationRoute.js
@@ -1,44 +1,66 @@
-const express = require("express");
+const express = require('express');
 
-const { createDonation, getAllDonation, deleteDonation, updateDonation, getDonation, myDonation, attachDispatchRiders, verifyPickedUp } = require ("../controllers/donationController");
-const auth = require ("../controllers/authController");
+const {
+  createDonation,
+  getAllDonation,
+  deleteDonation,
+  updateDonation,
+  getDonation,
+  myDonation,
+  attachDispatchRiders,
+  verifyPickedUp,
+} = require('../controllers/donationController');
 
- class DonationRouter {
-   constructor(router) {
-     this.router = router;
-   }
-   
-   apiRoute() {
-     // all route from here are authenticated
-     this.router.use(auth.authenticate());
-     
-     // Logged in user donation
-     this.router
-      .route("/my-donations")
-      .get(auth.authorize("volunteer", "donor"), myDonation);
-      
-     this.router
-      .route("/dispatch-rider")
-      .patch(auth.authorize("admin"), attachDispatchRiders);
-      
-     this.router
-      .route("/verify-picked-up")
-      .patch(auth.authorize("admin", "dispatch rider"), verifyPickedUp);
-     
-     this.router
-      .route("/")
-      .post(auth.authorize("admin", "volunteer", "donor"), createDonation)
-      .get(auth.authorize("admin"), getAllDonation);
-      
-      this.router
-      .route("/:id")
+const {
+  createMonetaryDonation,
+  verifyMonetaryDonation,
+} = require('../controllers/monetaryDonationController');
+
+const auth = require('../controllers/authController');
+
+class DonationRouter {
+  constructor(router) {
+    this.router = router;
+  }
+
+  apiRoute() {
+    // All route from here are authenticated
+    this.router.use(auth.authenticate());
+
+    // Logged in user donation
+    this.router
+      .route('/my-donations')
+      .get(auth.authorize('volunteer', 'donor'), myDonation)
+
+    this.router
+      .route('/dispatch-rider')
+      .patch(auth.authorize('admin'), attachDispatchRiders)
+
+    this.router
+      .route('/verify-picked-up')
+      .patch(auth.authorize('admin', 'dispatch rider'), verifyPickedUp)
+
+    this.router
+      .route('/')
+      .post(auth.authorize('admin', 'volunteer', 'donor'), createDonation)
+      .get(auth.authorize('admin'), getAllDonation)
+
+    this.router
+      .route('/monetary')
+      .post(auth.authorize('admin', 'volunteer', 'donor'), createMonetaryDonation)
+
+    this.router
+      .route('/monetary/verify')
+      .post(auth.authorize('admin', 'volunteer', 'donor'), verifyMonetaryDonation)
+
+    this.router
+      .route('/:id')
       .get(getDonation)
       .patch(updateDonation)
-      .delete(deleteDonation);
-      
+      .delete(deleteDonation)
+
     return this.router;
-   }
- }
- 
-  module.exports = new DonationRouter(express.Router()).apiRoute();
- 
+  }
+}
+
+module.exports = new DonationRouter(express.Router()).apiRoute();
