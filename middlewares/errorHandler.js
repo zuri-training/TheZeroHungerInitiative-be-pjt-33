676241f0f3e18801e4b399db2fe608162526f9a8
omzi <obiohaomezibe@gmail.com>
2021-06-29 13:07:49 +0100

    Add formatted errors functionality for users
    Made the errorHandler send formatted errors to the user in production environment

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,3 +1,5 @@
+const AppError = require('../utils/appError');
+
 const sendErrorInDev = (errorObject, res) => {
   console.log('❌', errorObject);
   res.status(errorObject.statusCode).json({
@@ -6,16 +8,34 @@ const sendErrorInDev = (errorObject, res) => {
 }
 
 const sendErrorInProduction = (errorObject, res) => {
-  if (errorObject.isOperational) {
-    res.status(errorObject.statusCode).json({
-      status: errorObject.status, message: errorObject.message
-    })
-  } else {
-    console.log('💥', err);
-    res.status(errorObject.statusCode).json({
-      status: errorObject.status, message: 'Something went wrong. Please try again later!'
-    })
+  let details;
+
+  // Mainly handles invalid data types. Can handle bad ObjectId
+  if (errorObject.name === 'CastError') {
+    errorObject = new AppError('Invalid data type provided', 400);
   }
+
+  // Mongoose duplicate key error
+  if (errorObject.name === 'MongoError' && errorObject.code === 11000) {
+    let duplicateError = errorObject.message.split('dup key: ').pop();
+    duplicateError = duplicateError.slice(3 - 1, duplicateError.length - 3).split(': "');
+    
+    details = { [duplicateError.shift()]: duplicateError.pop() }
+    errorObject = new AppError('A record exists with some of your entered values', 400);
+  }
+
+  // Mongoose validation error
+  if (errorObject.name === 'ValidationError') {
+    details = Object.entries(errorObject.errors).reduce((previous, [key, value]) => {
+      return { ...previous, [key]: value.properties.message }
+    }, {});
+    
+    errorObject = new AppError('Validation failed. Please enter all required values correctly', 400);
+  }
+
+  res.status(errorObject.statusCode).json({
+    status: errorObject.status, message: errorObject.message, details
+  })
 }
 
 module.exports = (errorObject, req, res, next) => {
